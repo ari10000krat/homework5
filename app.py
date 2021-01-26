07d7c4cb5557e86bf9c68f7ebc2c4ec18d9cecf2
glebPC <ksendziuk.gleb@gmail.com>
2021-01-26 08:46:23 +0200

    Add Calculating the number of astronauts

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from flask import Flask
 from faker import Faker
 import csv
 from tabulate import tabulate
+import requests
 
 app = Flask(__name__)
 
 
-@app.route('/generate-users/<int:countOfUsers>/')
-def generate_users(countOfUsers):  # http://127.0.0.1:5000/generate-users/5/ #TODO удалить
+@app.route('/generate-users/<int:countOfUsers>/') # http://127.0.0.1:5000/generate-users/5/ #TODO удалить
+def generate_users(countOfUsers):
     userData = []
     userInfo = ['Name', 'E-mail']
     fake = Faker(['ru_RU'])
@@ -33,6 +34,11 @@ def analize_csv():
                f'Total values in file (strings of data): {count - 1}<br>' \
                f'Average height: {round(SummOfHeights / (count - 1), 2)} cm<br>' \
                f'Average weight: {round(SummOfWeights / (count - 1), 2)} kg<br>'
+
+@app.route('/space/') #http://127.0.0.1:5000/space/
+def output_space_information():
+    r = requests.get('http://api.open-notify.org/astros.json')
+    return f"Number of astronauts: { r.json()['number']}"
 
 
 if __name__ == '__main__':
